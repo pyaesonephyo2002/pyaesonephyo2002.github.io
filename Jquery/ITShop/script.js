b4048ac55fba1a0f04c9b0761c4d1b76bdba7f17
pyaesonephyo2002 <pyaesonephyo1818@gmail.com>
2024-08-19 21:56:20 +0700

    update

--- Jquery/ITShop/script.js
+++ Jquery/ITShop/script.js
@@ -1,42 +1,65 @@
 $(document).ready(function(){
+    getData();
+    
+    function getData() {
+        let itemsString = localStorage.getItem('shops');
+        if(itemsString){
+           let itemsArray = JSON.parse(itemsString);
+           let data = '';
+           let j = 1;
+           
+           $.each(itemsArray, function(i, v){
+               data += `<tr>
+                   <td>${j++}</td>
+                   <td>${v.name}</td>
+                   <td>${v.price}</td>
+                   <td>${v.qty}</td>
+                   <td>${v.price * v.qty}</td>
+               </tr>`;
+           });
+           data += `<tr>
+            <td>Total</td>
+            <td></td>
+           </tr>`
+           $('#tbody').html(data);
+
+
+        }
+    }
     
     $('.addToCart').click(function(){
-        // alert("hello");
         let id = $(this).data('id');
         let name = $(this).data('name');
         let price = $(this).data('price');
 
-        console.log(id,name,price);
-
-
-
-        let items ={
+        let items = {
             id: id,
             name: name,
             price: price,
             qty: 1
-        }
+        };
 
         let itemsString = localStorage.getItem('shops');
         let itemsArray;
         if(itemsString == null) {
             itemsArray = [];
-        }else{
+        } else {
             itemsArray = JSON.parse(itemsString);
         }
-        let status= false;
-        $.each(itemsArray, function(i,v){
-            if(v.id ==id){
+
+        let status = false;
+        $.each(itemsArray, function(i, v){
+            if(v.id == id){
                 v.qty++;
                 status = true;
             }
-        })
+        });
+
         if(status == false) {
             itemsArray.push(items);
         }
 
         let itemsData = JSON.stringify(itemsArray);
         localStorage.setItem('shops', itemsData);
-
-    })
-})
+    });
+});
